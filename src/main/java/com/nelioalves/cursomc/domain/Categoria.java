503dbc586616bf80a9c119ab2d72cb99b159d625
Sanmuell <sanmuell.i@gmail.com>
2021-05-19 13:39:55 -0300

    Criando repository e Service para Categoria

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -10,28 +10,21 @@ import javax.persistence.Id;
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private String nome;
-	
-	
+
 	public Categoria() {
-		
+
 	}
 
-	public Categoria(Integer id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-	}
-
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -40,6 +33,12 @@ public class Categoria implements Serializable {
 	}
 
 	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria(Long id, String nome) {
+		super();
+		this.id = id;
 		this.nome = nome;
 	}
 
@@ -68,6 +67,4 @@ public class Categoria implements Serializable {
 		return true;
 	}
 
-	
-	
 }
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,30 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@GetMapping
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(1, "Informatica");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+
+	@Autowired
+	private CategoriaService categoriaService;
+
+	@GetMapping(value = "/{id}")
+	@ResponseBody
+	public ResponseEntity<?> buscar(@PathVariable Long id) {
+		Categoria obj = categoriaService.buscar(id);
+		return ResponseEntity.ok(obj);
+
 	}
 
 }
