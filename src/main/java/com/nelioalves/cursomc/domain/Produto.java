92b1c89bacc26efa4db9e7e822740a71dcc4d5ba
Sanmuell <sanmuell.i@gmail.com>
2021-05-20 10:40:05 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -7,22 +7,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	private Double endereco;
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = 
+	@JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name= "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
 
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	public Produto() {
 
-	public Categoria() {
+	}
 
+	public Produto(Long id, String nome, Double endereco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;
 	}
 
 	public Long getId() {
@@ -41,10 +53,20 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public Categoria(Long id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
+	public Double getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Double endereco) {
+		this.endereco = endereco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -63,21 +85,13 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
 	}
 
 }
